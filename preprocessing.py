027001b9887b5b7047a6b7491b9a6ce74b8c352f
Arvind <DellPC@ARV.local>
2022-04-18 00:37:09 -0500

    preprocessing of comments and happiness score evaluation

--- preprocessing.py
+++ preprocessing.py
@@ -3,47 +3,65 @@ from better_profanity import profanity
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 
-def training_model(reviews):
-    print("hello")
-    data = ["I love you", "I hate you"]
-    sentimentIntensityAnalyzer = SentimentIntensityAnalyzer()
-    for r in reviews:
-        words: list[str] = nltk.word_tokenize(r)
-        print(sentimentIntensityAnalyzer.polarity_scores(r))
+from model import Professor, University
 
-        # fd = nltk.FreqDist(words)
-        # lower_fd = nltk.FreqDist([w.lower() for w in fd])
-        # lower_fd.tabulate()
-        # print(fd.most_common(3))
 
-    #print(model.summary())
+def get_happiness_score_professor(professor_name):
+    sentiment_intensity_analyzer = SentimentIntensityAnalyzer()
+    professor = Professor.objects(name=professor_name).first()
+    # print(professor.comments)
+    count = 0
+    total = 0
+    old_range = 2
+    new_range = 5
+    processed_comments = preprocess_comments(professor.comments)
+    for comment in processed_comments:
+        old_value = sentiment_intensity_analyzer.polarity_scores(comment)["compound"]
+        new_value = ((old_value - (-1)) * new_range) / old_range
+        total = total + new_value
+        count = count + 1
+
+    if count > 0:
+        total = total / count
+    else:
+        total = 0
+
+    return total
+
+
+def get_happiness_score_university(university_name):
+    sentiment_intensity_analyzer = SentimentIntensityAnalyzer()
+    university = University.objects(title=university_name).first()
+
+    professors_list = list(Professor.objects(university=university))
+    count = 0
+    total = 0
+    for professor in professors_list:
+        curr_professor_rating = get_happiness_score_professor(professor.name)
+        if curr_professor_rating != 0:
+            total += curr_professor_rating
+            count += 1
+
+    print(total)
+    return total / count
 
 
 def preprocess_comments(initial_reviews):
     final_reviews = []
     stopwords = nltk.corpus.stopwords.words("english")
-
+    # initial_reviews = Professor.objects(pid=115496).first().comments
     for s in initial_reviews:
         refined_text = profanity.censor(s)
         tokens = [word for word in refined_text.split() if word.lower() not in stopwords]
         temp = ' '.join(tokens)
         final_reviews.append(temp)
 
-    print(final_reviews)
     return final_reviews
 
 
 if __name__ == "__main__":
-    reviews = [
-        "Relatively fucked-up class, most stupid assignments could be completed by following step by step instructions from the "
-        "PDFs",
-        "13-14 assignments but are relatively easy. Can refer the textbook for the course and the lectures are also "
-        "good. No exams. Grading based on assignments, guided project and one research highlight (a custom project "
-        "based on one research paper or summary of 5 research papers). Easy A.",
-        "I'm currently in NLP and I have to say I'm genuinely disappointed. I have a deep love for math and for the "
-        "beauty of AI. Unfortunately, I've spent most of my time in this class writing plans for a chatbot design("
-        "worst project ever) and doing trial and error on assignments. This class can outright be taught better "
-        "across the board."]
 
-    final_reviews = preprocess_comments(reviews)
-    training_model(final_reviews)
+    print(get_happiness_score_professor("Salvatore Ferrugia"))
+    # print(get_happiness_score_university("Adelphi University"))
+    # _reviews = preprocess_comments()
+    # training_model(_reviews)
